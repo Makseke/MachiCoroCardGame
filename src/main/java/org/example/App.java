728762de5184e402f78a894836e68e000fc4b577
Maksim Navoichyk <makseke@mail.ru>
2023-12-13 23:07:57 +0300

    update connection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,9 +17,6 @@ import java.util.Scanner;
 @Setter
 public class App
 {
-
-
-
     public static void main( String[] args )
     {
         InetAddress localhost;
@@ -28,8 +25,6 @@ public class App
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
-
-
 
         Scanner scanner = new Scanner(System.in);
 
@@ -46,10 +41,6 @@ public class App
         else {
             System.out.println("ERROR");
         }
-
-
-
-
     }
 
     private static void startServer() {
@@ -58,14 +49,26 @@ public class App
         server.getKryo().register(TextRequestTO.class);
 
         server.addListener(new Listener() {
+
+            @Override
+            public void connected(Connection connection) {
+                TextResponseTO welcomeMessage = new TextResponseTO();
+                welcomeMessage.setText("Welcome to the server!");
+                connection.sendTCP(welcomeMessage);
+                connection.setTimeout(0);
+                connection.setKeepAliveTCP(5000);
+            }
+
+            @Override
             public void received(Connection connection, Object object) {
                 if (object instanceof TextRequestTO) {
                     TextRequestTO request = (TextRequestTO) object;
-
-                    // Обработка запроса и отправка ответа
+                    
                     TextResponseTO response = new TextResponseTO();
                     response.setText("Received message: " + request.getText());
                     connection.sendTCP(response);
+
+                    System.out.println(response.getText());
                 }
             }
         });
@@ -88,19 +91,24 @@ public class App
         String ip = scanner.nextLine();
 
         client.addListener(new Listener() {
+            @Override
             public void received(Connection connection, Object object) {
                 if (object instanceof TextResponseTO) {
                     TextResponseTO response = (TextResponseTO) object;
                     System.out.println("Server response: " + response.getText());
                 }
             }
+
+            @Override
+            public void connected(com.esotericsoftware.kryonet.Connection connection) {
+                connection.setTimeout(0);
+                connection.setKeepAliveTCP(5000);
+            }
         });
 
         try {
             client.start();
             client.connect(5000, ip, 54555, 54777);
-
-            // Создание и отправка запроса на сервер
             TextRequestTO request = new TextRequestTO();
             request.setText("Hello, server!");
             client.sendTCP(request);
